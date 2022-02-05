b1dd37ab31bc377d60c829a035d0f02b86115530
Andrew <andrijprudnikov@gmail.com>
2022-02-05 18:55:57 +0200

    Added manager card

---
+++ src/components/cards/ManagerListCard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./index.css";
+
+const ManagerListCard = ({role, manager}) => {
+    return (
+        <div className="list-card-manager">
+
+        </div>
+    )
+}
+
+export default ManagerListCard;
--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import "./index.css";
+import ManagerListCard from "../components/cards/ManagerListCard";
 
 const LandingPage = () => {
     return (
         <div className="page-landing">
-
+            <ManagerListCard />
         </div>
     )
 }
